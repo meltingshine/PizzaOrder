954acf307f2c723bd8732c00f7882b797745f909
meltingshine <meltingshine@gmail.com>
2020-06-10 20:58:00 +0900

    <리스트뷰> 1.데이터클래스 2.XML 한줄 만듬 3. circleImageView 라이브러리 넣음

---
+++ app/src/main/java/com/example/pizzaorder/Datas/ChickenStoresData.kt
@@ -0,0 +1,6 @@
+package com.example.pizzaorder.Datas
+
+class ChickenStoresData(val name:String, val tel:String, val logo:String){
+
+
+}
---
+++ app/src/main/java/com/example/pizzaorder/Datas/PizzaStore.kt
@@ -0,0 +1,10 @@
+package com.example.pizzaorder.Datas
+
+
+class PizzaStore(
+    val name:String,
+    val phoneNum:String,
+    val logoUrl:String){
+
+
+}
